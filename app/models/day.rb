f3cbc13cb0354229e6cb5a11a6d145736df645ff
Manabu-Ito9772 <m.ito09017@gmail.com>
2021-05-30 14:23:02 +0900

    日付数/ブロック数上限設定

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,5 +1,15 @@
 class Day < ApplicationRecord
+  MAX_DAYS_COUNT = 14
+
   has_many :blocks, dependent: :destroy
   has_many :ordered_blocks, -> { order('position asc') }, class_name: :Block, dependent: :destroy
   belongs_to :article
+
+  validate :days_count_must_be_within_limit
+
+  private
+
+  def days_count_must_be_within_limit
+    errors.add(:base, "days count limit: #{MAX_DAYS_COUNT}") if article.days.count >= MAX_DAYS_COUNT
+  end
 end
