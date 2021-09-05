d2067093acefd1c8fd3a972409168e6319ddb01a
Manabu-Ito9772 <m.ito09017@gmail.com>
2021-09-05 15:57:49 +0900

    JWT期限切れ時のバグ修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,14 +5,4 @@ class Favorite < ApplicationRecord
   validates :user_id, presence: true
   validates :article_id, presence: true
   validates :user_id, uniqueness: { scope: :article_id }
-
-  def self.get_favorite_articles(favorites)
-    favorite_articles = []
-
-    favorites.each do |favo|
-      favorite_articles.push(favo.article) if favo.article.status == 'published'
-    end
-
-    favorite_articles
-  end
 end
